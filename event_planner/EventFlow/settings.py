6ca7b009a709494b30f6f75947241acfdf67f5f6
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-27 09:30:32 +0100

    added installed apps in settings file of my project folder

--- event_planner/EventFlow/settings.py
+++ event_planner/EventFlow/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',  # Added for API documentation
+    # Local apps
+    'users',
+    'events',
+    'core',
 ]
 
 MIDDLEWARE = [
