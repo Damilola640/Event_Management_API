d904f610c2c379e6d1695b385737756d01b4daf9
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-29 12:38:38 +0100

    updated settings

--- event_planner/EventFlow/settings.py
+++ event_planner/EventFlow/settings.py
@@ -129,8 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), # Correctly points to the 'static' folder of the project root
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
