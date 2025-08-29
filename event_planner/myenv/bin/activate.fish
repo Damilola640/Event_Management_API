7782cb837dccd126dd8f7947dd44050ed45778df
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-29 18:13:33 +0100

    renamed my repository

--- event_planner/myenv/bin/activate.fish
+++ event_planner/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/clement/Event_Management-API/event_planner/myenv
+set -gx VIRTUAL_ENV /home/clement/Event_Management_API/event_planner/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
