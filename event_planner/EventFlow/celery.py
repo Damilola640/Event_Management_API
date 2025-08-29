eb41d30fb245f0a3e82e7b16fd9821635da49424
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-29 18:19:29 +0100

    debugging

--- event_planner/EventFlow/celery.py
+++ event_planner/EventFlow/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event_Management-API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event_Management_API.settings')
 
-app = Celery('Event_Management-API')
+app = Celery('Event_Management_API')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
