46a0731cea29ee75fbfdbb656fb79e3c36a335ae
Damilola640 <Olatoyeclement531@gmail.com>
2025-10-06 07:07:04 +0100

    combining all default hosts into a comma-separated string

--- event_planner/EventFlow/settings.py
+++ event_planner/EventFlow/settings.py
@@ -28,9 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', 'eventflow-b919.onrender.com', '127.0.0.1,localhost', cast=Csv())
+# Combined all default hosts into a single comma-separated string
+default_hosts = 'eventflow-b919.onrender.com,127.0.0.1,localhost'
 
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=default_hosts, cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
