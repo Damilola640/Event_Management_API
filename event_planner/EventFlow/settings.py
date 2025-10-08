40632be6d5f9d25f49c4dc90d0b0859c2734c74a
Damilola640 <Olatoyeclement531@gmail.com>
2025-10-08 16:10:58 +0100

    debug set to false in settings to clear trailing dissallowed host in production

--- event_planner/EventFlow/settings.py
+++ event_planner/EventFlow/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False #config('DEBUG', default=False, cast=bool)
 
 # Combined all default hosts into a list of strings
 default_hosts = ['eventflow-b919.onrender.com', '127.0.0.1', 'localhost']
