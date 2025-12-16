edc3f5090d8b67d7443991042ebd0a3df0d0294a
Damilola640 <Olatoyeclement531@gmail.com>
2025-12-16 03:29:01 +0100

    Added Eventitem interface to define event structure

---
+++ Frontend/event-flow/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Eventitem {
+    id: number;
+    name: string;
+    description: string;
+    location: string;
+    date: string;
+    time: string;
+    category: string;
+    tags: string[];
+    image: string;
+  }
